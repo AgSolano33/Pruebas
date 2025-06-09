407675f290d3b4c4bbbd8e79df036120c70b92f4
alan erives <alanerives19@gmail.com>
2025-06-08 18:42:57 -0600

    fix(react-tool)

--- components/LayoutClient.js
+++ components/LayoutClient.js
@@ -7,7 +7,6 @@ import { Crisp } from "crisp-sdk-web";
 import { SessionProvider } from "next-auth/react";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "react-hot-toast";
-import { Tooltip } from "react-tooltip";
 import config from "@/config";
 
 // Crisp customer chat support:
@@ -49,8 +48,7 @@ const CrispChat = () => {
 // 1. SessionProvider: Allow the useSession from next-auth (find out if user is auth or not)
 // 2. NextTopLoader: Show a progress bar at the top when navigating between pages
 // 3. Toaster: Show Success/Error messages anywhere from the app with toast()
-// 4. Tooltip: Show tooltips if any JSX elements has these 2 attributes: data-tooltip-id="tooltip" data-tooltip-content=""
-// 5. CrispChat: Set Crisp customer chat support (see above)
+// 4. CrispChat: Set Crisp customer chat support (see above)
 const ClientLayout = ({ children }) => {
   return (
     <>
@@ -66,12 +64,6 @@ const ClientLayout = ({ children }) => {
           toastOptions={{
             duration: 3000,
           }}
-        />
-
-        {/* Show tooltips if any JSX elements has these 2 attributes: data-tooltip-id="tooltip" data-tooltip-content="" */}
-        <Tooltip
-          id="tooltip"
-          className="z-[60] !opacity-100 max-w-sm shadow-lg"
         />
 
         {/* Set Crisp customer chat support */}
